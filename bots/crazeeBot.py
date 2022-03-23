dc8e2a7fce0deaeae44182f237c0c76d5c124a63
Yisrael Hessler <yisrael.hessler@gmail.com>
2022-03-23 13:55:01 +0200

    Fixed The Simple Bot Getting Stuck

--- bots/crazeeBot.py
+++ bots/crazeeBot.py
@@ -5,24 +5,20 @@ import numpy as np
 class CrazeeSimpleBot(bots.api.Bot):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.moves = self.gen_possible_moves()
 
     def bot_logic(self, api):
-        moves = self.gen_possible_moves()
         #print(f"CrazeeBot: id {self.bot_id}, turn:{api.turn}, score:{api.get_score(self.bot_id)}, pos:{api.players_location[self.bot_id]}")
-        board = api.board
         my_pos = api.players_location[self.bot_id]
-        for move in moves:
-
+        for i, move in enumerate(self.moves):
+            #print(f"{i}:{move}")
             dest = my_pos + move
-            #print(f"Dest:{dest}")
-            #fix this
             if api.location_in_array(dest, api.players_location):
                 #print(f"{move} has a player")
                 continue
-
             if api.check_valid_move(self.bot_id, move):
                 if api.check_cell_color(self.bot_id, dest):
-                    #print(f"{move} is my color {board[dest[0], dest[1]]}")
+                    #print(f"{move} is my color {api.check_cell_color(self.bot_id, dest)}")
                     continue
                 #print(f"Crazee: Found Valid Move {move}")
                 return move
@@ -31,7 +27,16 @@ class CrazeeSimpleBot(bots.api.Bot):
         return [0, 0]
 
     def gen_possible_moves(self):
-        arr = np.array(np.meshgrid([0, 0], [1, 1], [-1, -1])).T.reshape(-1, 2)
-        return np.append(arr, np.array([[-1, -1], [1, 1]]), axis=0)
+
+        return np.array([[0, 0],
+                         [0, 1],
+                         [1, 0],
+                         [1, 1],
+                         [0, -1],
+                         [-1, 0],
+                         [-1, -1],
+                         [1, -1],
+                         [-1, 1],
+                         ])
 
 
